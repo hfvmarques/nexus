7ac60f57aac90a4baf6e3a1b99fc0bdbbfde1d01
Henrique Marques <hfvmarques@gmail.com>
2025-05-07 15:41:53 +0000

    feat: adds rspec as test framework and sample tests

---
+++ spec/config/routes_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe 'routes', type: :routing do
+  it { expect(get('/up')).to route_to('rails/health#show') }
+end
---
+++ spec/zeitwerk_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+# This test is to ensure that Zeitwerk is loading all folders and files.
+#
+describe 'Zeitwerk' do
+  it { expect { Zeitwerk::Loader.eager_load_all }.not_to raise_error }
+end
